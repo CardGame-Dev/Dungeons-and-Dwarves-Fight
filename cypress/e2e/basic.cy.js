2653b9a5c5e20cc661c1aa3da0b170c3ff7533fd
CardGame-Dev <125687938+CardGame-Dev@users.noreply.github.com>
2023-02-17 14:53:18 -0300

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,6 +4,6 @@ describe('sample render test', () => {
   });
 
   it('displays the title text', () => {
-    cy.get('h1').contains('Teste-Nome');
+    cy.get('h1').contains('Next.js Blog Theme');
   });
 });
